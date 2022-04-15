589176dd38ffd2ab8fd2ec5a733adf8293570a3f
Nataniel Santos <nathan.santos159@hotmail.com>
2022-04-15 16:15:49 -0300

    Requisito 4 - Cria classe Model generica

---
+++ src/models/index.ts
@@ -0,0 +1,26 @@
+import { Model as M, Document } from 'mongoose';
+import { Model } from '../interfaces/ModelInterface';
+
+abstract class MongoModel<T> implements Model<T> {
+  constructor(protected model: M<T & Document>) { }
+
+  create = async (obj: T): Promise<T> => this.model.create({ ...obj });
+
+  read = async (): Promise<T[]> => this.model.find();
+
+  readOne = async (id: string): Promise<T | null> =>
+    this.model.findOne({ _id: id });
+
+  update = async (str: string, obj: T): Promise<T | null> => {
+    const res = await this.model.updateOne({ _id: str, obj });
+    return res.modifiedCount ? obj : null;
+  };
+
+  delete = async (str: string): Promise<T | null> => {
+    const obj = await this.model.findOne({ _id: str });
+    const res = await this.model.deleteOne({ _id: str });
+    return res.deletedCount ? obj : null;
+  };
+}
+
+export default MongoModel;
